13d3624588b07fd326953d5ba4cca0b7aae5dc7d
Bishwash Khanal <lionelbishwash11@gmail.com>
2023-04-02 21:13:19 +0545

    added resnet50 as backbone

---
+++ backbone.py
@@ -0,0 +1,111 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Bottleneck(nn.Module):
+
+    def __init__(self, in_channels, mid_channels, out_channels, stride=1):
+        super(Bottleneck, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels, mid_channels, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(mid_channels)
+
+        self.conv2 = nn.Conv2d(mid_channels, mid_channels, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(mid_channels)
+
+        self.conv3 = nn.Conv2d(mid_channels, out_channels, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channels)
+
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = None
+        if stride != 1 or in_channels != out_channels:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+class ResNet50(nn.Module):
+
+    def __init__(self, num_classes=1000):
+        super(ResNet50, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = nn.Sequential(
+            Bottleneck(64, 64, 256),
+            Bottleneck(256, 64, 256),
+            Bottleneck(256, 64, 256)
+        )
+        self.layer2 = nn.Sequential(
+            Bottleneck(256, 128, 512),
+            Bottleneck(512, 128, 512),
+            Bottleneck(512, 128, 512),
+            Bottleneck(512, 128, 512)
+        )
+        self.layer3 = nn.Sequential(
+            Bottleneck(512, 256, 1024),
+            Bottleneck(1024, 256, 1024),
+            Bottleneck(1024, 256, 1024),
+            Bottleneck(1024, 256, 1024),
+            Bottleneck(1024, 256, 1024),
+            Bottleneck(1024, 256, 1024)
+        )
+        self.layer4 = nn.Sequential(
+            Bottleneck(1024, 512, 2048),
+            Bottleneck(2048, 512, 2048),
+            Bottleneck(2048, 512, 2048)
+        )
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(2048, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+if __name__ == "__main__":
+    # Create an instance of the ResNet50 model
+    model = ResNet50()
+
+    # Get the total number of trainable parameters in the model
+    total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+    print(f"Number of trainable parameters in ResNet50: {total_params}")
