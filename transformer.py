6b7430f00f1e422353d3f4ebed163cdc66bdfbde
Bishwash Khanal <lionelbishwash11@gmail.com>
2023-04-04 17:12:33 +0545

    added tranformer

--- transformer.py
+++ transformer.py
@@ -7,7 +7,7 @@ class FullyConnected(nn.Module):
     def __init__(self, embedding_dim, fully_connected_dim):
         super(FullyConnected, self).__init__()
         self.fc1 = nn.Linear(embedding_dim, fully_connected_dim)
-        self.relu = nn.GELU()
+        self.relu = nn.ReLU()
         self.fc2 = nn.Linear(fully_connected_dim, embedding_dim)
 
     def forward(self, x):
@@ -30,6 +30,31 @@ class LayerNorm(torch.nn.Module):
         normalized = (x - mean) / (std + self.eps)
         
         return self.scale * normalized + self.bias
+
+class PositionalEmbedding(nn.Module):
+    def __init__(self, d_model, max_seq_len=50):
+        super().__init__()
+        self.embed_dim = int(d_model / 2)
+        self.row_embed = nn.Embedding(max_seq_len, self.embed_dim)
+        self.col_embed = nn.Embedding(max_seq_len, self.embed_dim)
+
+    def reset_parameters(self):
+        nn.init.uniform_(self.row_embed.weight)
+        nn.init.uniform_(self.col_embed.weight)
+
+    def forward(self, x):
+        h, w = x.size()[-2:]
+        
+        i = torch.arange(w, device=x.device)
+        j = torch.arange(h, device=x.device)
+        
+        x_embed = self.col_embed(i) # w, embed_dim
+        y_embed = self.row_embed(j) # h, embed_dim
+
+        pos = torch.cat([x_embed.unsqueeze(0).repeat(h,1,1),
+                         y_embed.unsqueeze(1).repeat(1,w,1)
+                         ], dim=-1).permute(2,0,1).unsqueeze(0).repeat(x.shape[0],1,1,1)
+        return pos
 
 class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, d_model, dropout_rate=0.1, **kwargs):
@@ -122,7 +147,7 @@ class EncoderLayer(nn.Module):
             encoder_layer_out -- Tensor of shape (batch_size, input_seq_len, d_model)
         """
         # calculate Self-Attention using Multi-Head Attention
-        mha_output, _ = self.mha(x, x, x, mask)  # Self attention (batch_size, input_seq_len, d_model)
+        mha_output = self.mha(x, x, x, mask)  # Self attention (batch_size, input_seq_len, d_model)
 
         # skip connection
         # apply layer normalization on sum of the input and the attention output to get the output of the multi-head attention layer
@@ -150,6 +175,9 @@ class Encoder(nn.Module):
         self.num_layers = num_layers
         self.d_model = d_model
         
+        self.embedding = nn.Embedding(input_vocab_size, d_model)
+        self.pos_encoding = PositionalEmbedding(d_model, maximum_position_encoding)
+        
         self.dropout = nn.Dropout(dropout_rate)
         
         self.enc_layers = nn.ModuleList([EncoderLayer(num_heads, d_model, fully_connected_dim,
@@ -166,7 +194,16 @@ class Encoder(nn.Module):
                     treated as part of the input
         Returns:
             encoder_out -- Tensor of shape (batch_size, input_seq_len, embedding_dim)
-        """
+        """        
+        # Embedding lookup and scaling
+        x = self.embedding(x) * math.sqrt(self.d_model)
+        
+        # Add position encoding to the input
+        x = self.pos_encoding(x)
+        
+        # Apply dropout to the input
+        x = self.dropout(x)
+        
         # Pass the input through each encoder layer
         for i in range(self.num_layers):
             x = self.enc_layers[i](x, mask)
@@ -175,3 +212,136 @@ class Encoder(nn.Module):
         
         return encoder_out
     
+class DecoderLayer(nn.Module):
+    """
+    The decoder layer is composed by an masked multi-head self-attention mechanism,
+    followed by a multi-head attention mechanism to the output of the encoder and a 
+    simple, position-wise fully connected feed-forward network. This architecture 
+    includes residual connections around all of the three sub-layers, followed by 
+    layer normalization.
+    """
+    def __init__(self, num_heads, d_model, fully_connected_dim,
+                 dropout_rate=0.1, layernorm_eps=1e-6, **kwargs):
+        super(DecoderLayer, self).__init__(**kwargs)
+
+        self.masked_mha = MultiHeadAttention(num_heads=num_heads,
+                                             d_model=d_model,
+                                             dropout_rate=dropout_rate)
+
+        self.mha = MultiHeadAttention(num_heads=num_heads,
+                                      d_model=d_model,
+                                      dropout_rate=dropout_rate)
+
+        self.ffn = FullyConnected(embedding_dim=d_model,
+                                  fully_connected_dim=fully_connected_dim)
+
+        self.layernorm1 = LayerNorm(d_model, eps=layernorm_eps)
+        self.layernorm2 = LayerNorm(d_model, eps=layernorm_eps)
+        self.layernorm3 = LayerNorm(d_model, eps=layernorm_eps)
+
+        self.dropout_ffn = nn.Dropout(dropout_rate)
+
+    def forward(self, x, encoder_output, look_ahead_mask=None, padding_mask=None):
+        """
+        Forward pass for the Decoder Layer
+        
+        Arguments:
+            x -- Tensor of shape (batch_size, target_seq_len, fully_connected_dim)
+            encoder_output -- Tensor of shape (batch_size, input_seq_len, embedding_dim)
+            look_ahead_mask -- Mask to ensure that the decoder does not attend to subsequent positions
+            padding_mask -- Boolean mask to ensure that the padding is not treated as part of the input
+        Returns:
+            decoder_layer_out -- Tensor of shape (batch_size, target_seq_len, embedding_dim)
+            attn_weights_block1 -- Tensor of shape (batch_size, num_heads, target_seq_len, target_seq_len)
+            attn_weights_block2 -- Tensor of shape (batch_size, num_heads, target_seq_len, input_seq_len)
+        """
+        # masked multi-head self-attention
+        mha_output1 = self.masked_mha(x, x, x, look_ahead_mask) # (batch_size, target_seq_len, fully_connected_dim)
+
+        # skip connection
+        skip_x_attention1 = self.layernorm1(x + mha_output1)
+
+        # multi-head attention on encoder output
+        mha_output2 = self.mha(skip_x_attention1, encoder_output, encoder_output, padding_mask) # (batch_size, target_seq_len, fully_connected_dim)
+
+        # skip connection
+        skip_x_attention2 = self.layernorm2(skip_x_attention1 + mha_output2)
+
+        # pass the output of the multi-head attention layer through a ffn
+        ffn_output = self.ffn(skip_x_attention2)
+
+        # apply dropout layer to ffn output during training
+        ffn_output = self.dropout_ffn(ffn_output)
+
+        # apply layer normalization on sum of the output from multi-head attention (skip connection) and ffn output to get the output of the decoder layer
+        decoder_layer_out = self.layernorm3(skip_x_attention2 + ffn_output)
+
+        return decoder_layer_out
+
+class Decoder(nn.Module):
+    """
+    The Decoder consists of N layers of DecoderLayer.
+    """
+    def __init__(self, num_layers, num_heads, d_model, fully_connected_dim, target_vocab_size,
+                 maximum_position_encoding, dropout_rate=0.1, layernorm_eps=1e-6):
+        super(Decoder, self).__init__()
+        
+        self.num_layers = num_layers
+        self.d_model = d_model
+        
+        self.embedding = nn.Embedding(target_vocab_size, d_model)
+        self.pos_encoding = PositionalEmbedding(d_model, maximum_position_encoding)
+        
+        self.decoder_layers = nn.ModuleList([
+            DecoderLayer(num_heads=num_heads,
+                         d_model=d_model,
+                         fully_connected_dim=fully_connected_dim,
+                         dropout_rate=dropout_rate,
+                         layernorm_eps=layernorm_eps)
+            for _ in range(num_layers)
+        ])
+        
+        self.dropout = nn.Dropout(dropout_rate)
+
+    def forward(self, x, enc_output, look_ahead_mask=None, padding_mask=None):
+        """
+        Forward pass for the Decoder.
+        
+        Arguments:
+            x -- Tensor of shape (batch_size, target_seq_len)
+            enc_output -- Tensor of shape (batch_size, input_seq_len, embedding_dim)
+            look_ahead_mask -- Boolean mask for the target sequence
+            padding_mask -- Boolean mask for the input sequence
+            
+        Returns:
+            decoder_output -- Tensor of shape (batch_size, target_seq_len, embedding_dim)
+            attention_weights -- Dictionary of attention weights for each decoder layer
+        """
+        x = self.embedding(x) # (batch_size, target_seq_len, d_model)
+        x *= torch.sqrt(torch.tensor(self.d_model).float())
+        x = self.pos_encoding(x)
+        x = self.dropout(x)
+        
+        for i in range(self.num_layers):
+            x = self.decoder_layers[i](x, enc_output, look_ahead_mask, padding_mask)
+        
+        return x
+
+class Transformer(nn.Module):
+    def __init__(self, num_encoder_layers, num_decoder_layers, d_model, num_heads, dff, input_vocab_size, target_vocab_size, max_seq_len, dropout_rate=0.1):
+        super(Transformer, self).__init__()
+
+        self.encoder = Encoder(num_encoder_layers, num_heads, d_model, dff, input_vocab_size, max_seq_len, dropout_rate)
+
+        self.decoder = Decoder(num_decoder_layers, num_heads, d_model, dff, target_vocab_size, max_seq_len, dropout_rate)
+
+        self.final_layer = nn.Linear(d_model, target_vocab_size)
+
+    def forward(self, inp, tar, enc_padding_mask=None, look_ahead_mask=None, dec_padding_mask=None):
+        enc_output = self.encoder(inp, enc_padding_mask)
+
+        dec_output = self.decoder(tar, enc_output, look_ahead_mask, dec_padding_mask)
+
+        final_output = self.final_layer(dec_output)
+
+        return final_output
